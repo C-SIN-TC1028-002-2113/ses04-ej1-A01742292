259d781b643363620a16481ac6526067de6a549d
A01742292 <A01742292@itesm.mx>
2021-08-20 20:15:04 +0000

    ejercicio

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
+    edad=float(input("Edad:"))
+    id=float(input("¿Tienes identificacion oficial? (s/n): "))
+    if edad>=18 and id=="s"
+    print("Tramite de licencia concedido")
+    elif edad<=0 or id!="s" or "n"
+    print("Respuesta incorrecta")
+    else
+    print("No cumples con los requisitos")
+        pass
 if __name__ == '__main__':
     main()
