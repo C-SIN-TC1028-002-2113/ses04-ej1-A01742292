d65a61454d723422597a9b822b8b98ff2d23f636
A01742292 <A01742292@itesm.mx>
2021-08-20 20:21:48 +0000

    ejercicio

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+    e1= int(input("Ingresa el primer numero"))
+    e2= int(input("Ingresa el segundo numero"))
+    e3= int(input("Ingresa el tercer numero"))
+
+    if e1<e2 and e1<e3:
+        print(e1)
+    elif e2<ez and e2<e3:
+        print(e2)
+    else e3<e1 and e3<e1:
+        print(e3)
     pass
 
 if __name__=='__main__':
