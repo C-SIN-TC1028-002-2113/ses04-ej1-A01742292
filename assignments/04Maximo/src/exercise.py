a032c4ec0d1e8c2c201606a0c07eec9ceed836ee
A01742292 <A01742292@itesm.mx>
2021-10-23 21:57:44 +0000

    ejercicio

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,16 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
-    e1= int(input("Ingresa el primer numero"))
-    e2= int(input("Ingresa el segundo numero"))
-    e3= int(input("Ingresa el tercer numero"))
+     num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    num3 = int(input("Ingresa el tercer número: "))
 
-    if e1 < e2 and e1<e3:
-        print(e1)
-    elif e2 < e1 and e2<e3:
-        print(e2)
-    else e3 < e1 and e3<e1:
-        print(e3)
-    pass
-
+    if num1>=num2 and num1>=num3:
+        print(num1)
+    elif num2>=num1 and num2>=num3:
+        print(num2)
+    else:
+        print(num3)
 if __name__=='__main__':
     main()
